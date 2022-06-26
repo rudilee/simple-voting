810f1d4f73d1817264d93682cbe49049c9340384
Rudi <rudi@kanayahijab.com>
2022-06-26 13:51:12 +0700

    Add database schema

---
+++ scripts/db.js
@@ -0,0 +1,13 @@
+const db = new Dexie('voting-db');
+db.version(1).stores({
+    elections: '++id, title, description',
+    candidates: '++id, name',
+    votes: '++id, candidateId',
+});
+db.version(2).stores({
+    votes: '++id, electionId, candidateId',
+});
+db.version(3).stores({
+    elections: '++id, title, description, maxVotes',
+    candidates: '++id, name, electionId',
+});
